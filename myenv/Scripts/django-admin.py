b2ff4b136388b666f9092f515c6b781a9f411dce
munira449 <munirahuda449@gmail.com>
2023-11-27 22:06:59 +0600

    commit 1

---
+++ myenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\User\Desktop\Library_Management_Django-master\myenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
